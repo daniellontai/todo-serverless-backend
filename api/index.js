4faf62431d7291dbd3a3fc4497c6e38e46abaeab
Lontai Dániel <lontai.daniel@gmail.com>
2024-02-25 23:30:42 +0100

    removed Cache-Control middleware

--- api/index.js
+++ api/index.js
@@ -20,12 +20,12 @@ app.use(cors({
     credentials: true,
 }));
 // Add headers before the routes are defined
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
     // Cache control for Vercel, todo check
-    res.setHeader('Cache-Control', 's-max-age=3600, stale-while-revalidate');
+    //res.setHeader('Cache-Control', 's-max-age=3600, stale-while-revalidate');
     // Pass to next layer of middleware
     next();
-});
+});*/
 
 app.get('/api', (req, res) => {
     res.sendStatus(400);
