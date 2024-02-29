d9fe1369e29fa9e9b725ee31f8fa1afb758b23a6
Lontai Dániel <lontai.daniel@gmail.com>
2024-02-29 18:36:44 +0100

    removed headers from vercel.json, added necessary headers to setHeadersMiddleware

--- middleware/setHeaders.js
+++ middleware/setHeaders.js
@@ -1,5 +1,6 @@
 const setGeadersMiddleware = (req, res, next) => {
     res.setHeader('Cache-Control', 's-max-age=3600, stale-while-revalidate');
+    res.setHeader('Content-Security-Policy', "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self'; font-src 'self'; connect-src 'self'");
     next();
 }
 
